965d63b93a576ce53dfde299b34fafda79f34ec7
Fish Li（李奇峰） <liqifeng0503@163.com>
2018-10-25 11:51:20 +0800

    add RequestLogConsole

---
+++ test/ClownFish.RequestLogConsole/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ClownFish.Base;
+using ClownFish.HttpServer;
+
+namespace ClownFish.RequestLogConsole
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Init();
+
+            ServerHost httpHost = new ServerHost();
+            httpHost.Run();
+
+            Console.WriteLine("HTTP监听设置成功。");
+            Console.WriteLine("站点网址：" + httpHost.Option.HttpListenerOptions.First().ToUrl());
+            Console.WriteLine("EXIT: 退出模拟服务，cls: 清屏。");
+
+            while( true ) {
+                string input = Console.ReadLine();
+                if( input.Is("exit") ) {
+                    return;
+                }
+                if( input.Is("cls") ) {
+                    Console.Clear();
+                }
+            }
+        }
+
+
+        private static void Init()
+        {
+            string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
+
+            // 将当前进程添加到防火墙的允许列表中
+            NetFwTypeLib.FirewallHelper.AllowApplication(exePath);
+
+
+            // 强制指定【当前目录】，因为程序有可能是从计划任务中启动的，当前目录是Windows系统目录
+            Environment.CurrentDirectory = Path.GetDirectoryName(exePath);
+        }
+    }
+}
---
+++ test/ClownFish.RequestLogConsole/RequestLogHttpModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClownFish.Base;
+using ClownFish.Base.TypeExtend;
+using ClownFish.HttpServer.Result;
+using ClownFish.HttpServer.Utils;
+using ClownFish.HttpServer.Web;
+
+namespace ClownFish.RequestLogConsole
+{
+    public class RequestLogHttpModule : EventSubscriber<HttpModule>
+    {
+        public override void SubscribeEvent(HttpModule instance)
+        {
+            instance.BeginRequest += Instance_BeginRequest;
+        }
+
+        private void Instance_BeginRequest(object sender, EventArgs e)
+        {
+            HttpApplication app = (HttpApplication)sender;
+            NetHttpInfo httpInfo = NetHttpInfo.Create(app.Context);
+
+            Console.WriteLine(httpInfo.RequestText);
+            Console.WriteLine("\r\n");
+
+
+            TextResult result = new TextResult("server is OK. " + DateTime.Now.ToTimeString());
+            (result as IActionResult).Ouput(app.Context);
+
+            app.CompleteRequest();
+        }
+    }
+}
